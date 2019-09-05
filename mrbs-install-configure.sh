5b9e04de2748f575cccc2d0d4a2c0cd6c688ede3
Raul Tierno <raultm@gmail.com>
2019-09-05 11:47:11 +0200

    Update mrbs-install-configure.sh

--- mrbs-install-configure.sh
+++ mrbs-install-configure.sh
@@ -2,8 +2,8 @@
 # wget https://raw.githubusercontent.com/raultm/setup-mrbs/master/mrbs-install-configure.sh && sh ./mrbs-install-configure.sh  && rm ./mrbs-install-configure.sh
 
 # LAMP & MRBS
-wget https://raw.githubusercontent.com/raultm/setup-mrbs/master/setup-mrbs-ubuntu18.sh && sh ./setup-mrbs-ubuntu18.sh && rm ./setup-mrbs-ubuntu18.sh
+wget https://raw.githubusercontent.com/raultm/setup-mrbs/master/01_Ubuntu18_LAMP_mrbs.sh && sh ./01_Ubuntu18_LAMP_mrbs.sh && rm ./01_Ubuntu18_LAMP_mrbs.sh
 # MRBS Configuration
-wget https://raw.githubusercontent.com/raultm/setup-mrbs/master/configure-mrbs-ies.sh && sh ./configure-mrbs-ies.sh && rm ./configure-mrbs-ies.sh
+wget https://raw.githubusercontent.com/raultm/setup-mrbs/master/02_configure-mrbs-ies.sh && sh ./02_configure-mrbs-ies.sh && rm ./02_configure-mrbs-ies.sh
 # Solve Locale es_ES Problems
-wget https://raw.githubusercontent.com/raultm/setup-mrbs/master/locale.sh && sh ./locale.sh && rm ./locale.sh
+wget https://raw.githubusercontent.com/raultm/setup-mrbs/master/03_locale.sh && sh ./03_locale.sh && rm ./03_locale.sh
